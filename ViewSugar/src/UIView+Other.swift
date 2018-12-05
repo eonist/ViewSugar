c265a2840fc7571fffd6313b7fc39b4abe3e2686
Eonist <30n1st@gmail.com>
2018-12-05 08:47:03 +0100

    Add colorAtPoint method

--- ViewSugar/src/UIView+Other.swift
+++ ViewSugar/src/UIView+Other.swift
@@ -29,5 +29,27 @@ public extension UIView{
    public var scale:CGFloat {
       return sqrt(self.transform.a * self.transform.a + self.transform.c * self.transform.c)
    }
+   /**
+    * Returns color for point in UIView
+    */
+   public func color(point:CGPoint) -> UIColor {
+      let colorSpace:CGColorSpace = CGColorSpaceCreateDeviceRGB()
+      let bitmapInfo = CGBitmapInfo.init(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)//fromRaw()!
+      
+      var pixelData:[UInt8] = [0, 0, 0, 0]
+      
+      if let context = CGContext(data: &pixelData, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: bitmapInfo.rawValue) {
+         context.translateBy(x: -point.x, y: -point.y)
+         self.layer.render(in: context)
+      }
+      
+      let red:CGFloat = CGFloat(pixelData[0])/CGFloat(255.0)
+      let green:CGFloat = CGFloat(pixelData[1])/CGFloat(255.0)
+      let blue:CGFloat = CGFloat(pixelData[2])/CGFloat(255.0)
+      let alpha:CGFloat = CGFloat(pixelData[3])/CGFloat(255.0)
+      
+      let color:UIColor = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+      return color
+   }
 }
 #endif
