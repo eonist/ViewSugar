d6903f8cf5d122cea924d995ebde9ef8170b493f
Eonist <30n1st@gmail.com>
2019-02-03 17:56:47 +0100

    Minor syntax tweak

--- ViewSugar/src/UIView+Other.swift
+++ ViewSugar/src/UIView+Other.swift
@@ -6,6 +6,7 @@ import UIKit.UIView
 public extension UIView{
    /**
     * Creates UIImage from a view
+    * - Important: ⚠️️ if you get the "invalid context 0x0" error, make sure your View has a frame. view.bounds must not be .zerp
     */
    public var snapShot:UIImage? {
       UIGraphicsBeginImageContextWithOptions(self.frame.size, false, 0)
@@ -35,19 +36,19 @@ public extension UIView{
    public func color(point:CGPoint) -> UIColor {
       let colorSpace:CGColorSpace = CGColorSpaceCreateDeviceRGB()
       let bitmapInfo = CGBitmapInfo.init(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)//fromRaw()!
-      
+
       var pixelData:[UInt8] = [0, 0, 0, 0]
-      
+
       if let context = CGContext(data: &pixelData, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: bitmapInfo.rawValue) {
          context.translateBy(x: -point.x, y: -point.y)
          self.layer.render(in: context)
       }
-      
+
       let red:CGFloat = CGFloat(pixelData[0])/CGFloat(255.0)
       let green:CGFloat = CGFloat(pixelData[1])/CGFloat(255.0)
       let blue:CGFloat = CGFloat(pixelData[2])/CGFloat(255.0)
       let alpha:CGFloat = CGFloat(pixelData[3])/CGFloat(255.0)
-      
+
       let color:UIColor = UIColor(red: red, green: green, blue: blue, alpha: alpha)
       return color
    }
